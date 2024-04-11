c2e64302bf8928cc54ad799c598ceaf8061f8e3e
Neraky <felipecartaxo1@gmail.com>
2024-04-10 22:59:33 -0300

    Realizando ajustes nos comentários dos códigos

--- src/appconsole/Teste1.java
+++ src/appconsole/Teste1.java
@@ -1,9 +1,4 @@
 package appconsole;
-/**
- * SI - POO - Prof. Fausto Ayres
- * Teste da Fachada
- * 
- */
 
 import modelo.Evento;
 import modelo.Ingresso;
@@ -14,105 +9,128 @@ public class Teste1 {
 
 	public Teste1() {
 
-
 		try {
 			System.out.println("\ncriando participantes e convidados");
+
+			// Criando os participantes
 			Fachada.criarParticipante("1111", "01/01/1960");
 			Fachada.criarParticipante("2222", "01/01/2000");
 			Fachada.criarParticipante("3333", "01/01/2010");
+
+			// Criando os convidados
 			Fachada.criarConvidado("4444", "01/01/1960", "empresa1");
 			Fachada.criarConvidado("5555", "01/01/2000", "empresa2");
 			Fachada.criarConvidado("6666", "01/01/2010", "empresa3");
 			Fachada.criarConvidado("7777", "01/01/2010", "empresa3");
+
 			System.out.println("---------listagem de participantes e convidados -----");
-			for(Participante p : Fachada.listarParticipantes()) 
+
+			// Listando os participantes
+			for (Participante p : Fachada.listarParticipantes())
 				System.out.println(p);
 
 		} catch (Exception e) {
-			System.out.println("--->"+e.getMessage());
-		}		
+			System.out.println("--->" + e.getMessage());
+		}
 
 		try {
 			System.out.println("\ncriando eventos");
-			Fachada.criarEvento("24/11/2023","show da pisadinha",2,100.0);
-			Fachada.criarEvento("24/06/2024","show de sao joao",2,200.0);
-			Fachada.criarEvento("01/01/2024","fake",2,0.0);
+
+			// Criando os eventos
+			Fachada.criarEvento("24/11/2023", "show da pisadinha", 2, 100.0);
+			Fachada.criarEvento("24/06/2024", "show de sao joao", 2, 200.0);
+			Fachada.criarEvento("01/01/2024", "fake", 2, 0.0);
+
 			System.out.println("---------listagem de eventos");
-			for(Evento e : Fachada.listarEventos()) 
+
+			// Listando os eventos
+			for (Evento e : Fachada.listarEventos())
 				System.out.println(e);
 
 		} catch (Exception e) {
-			System.out.println("--->"+e.getMessage());
+			System.out.println("--->" + e.getMessage());
 		}
 
 		try {
 			System.out.println("\ncriando ingressos");
-			Fachada.criarIngresso(1,"1111","988001101");
-			Fachada.criarIngresso(1,"2222","988001102");
-			Fachada.criarIngresso(1,"3333","988001103");
-			Fachada.criarIngresso(1,"4444","988001104");
-			Fachada.criarIngresso(1,"5555","988001105");
-			Fachada.criarIngresso(1,"6666","988001106");
-			Fachada.criarIngresso(2,"1111","988001101");
-			Fachada.criarIngresso(2,"2222","988001102");
-			Fachada.criarIngresso(2,"3333","988001103");
-			Fachada.criarIngresso(2,"4444","988001104");
-			Fachada.criarIngresso(2,"5555","988001105");
-			Fachada.criarIngresso(2,"6666","988001106");
-			Fachada.criarIngresso(1,"7777","988001107");
+
+			// Criando os ingressos
+			Fachada.criarIngresso(1, "1111", "988001101");
+			Fachada.criarIngresso(1, "2222", "988001102");
+			Fachada.criarIngresso(1, "3333", "988001103");
+			Fachada.criarIngresso(1, "4444", "988001104");
+			Fachada.criarIngresso(1, "5555", "988001105");
+			Fachada.criarIngresso(1, "6666", "988001106");
+			Fachada.criarIngresso(2, "1111", "988001101");
+			Fachada.criarIngresso(2, "2222", "988001102");
+			Fachada.criarIngresso(2, "3333", "988001103");
+			Fachada.criarIngresso(2, "4444", "988001104");
+			Fachada.criarIngresso(2, "5555", "988001105");
+			Fachada.criarIngresso(2, "6666", "988001106");
+			Fachada.criarIngresso(1, "7777", "988001107");
+
 			System.out.println("---------listagem de ingressos");
-			for(Ingresso ing : Fachada.listarIngressos()) 
+
+			// Listando os ingressos
+			for (Ingresso ing : Fachada.listarIngressos())
 				System.out.println(
-						"cod="+ing.getCodigo()+ ", "+ ing.getTelefone() + ", preco=" +	ing.calcularPreco() + 
-						", evento preco=" + ing.getEvento().getPreco() + ", arrecadado=" + ing.getEvento().totalArrecadado() + 
-						", idade=" + ing.getParticipante().calcularIdade());
+						"cod=" + ing.getCodigo() + ", " + ing.getTelefone() + ", preco=" + ing.calcularPreco() +
+								", evento preco=" + ing.getEvento().getPreco() + ", arrecadado="
+								+ ing.getEvento().totalArrecadado() +
+								", idade=" + ing.getParticipante().calcularIdade());
 
 		} catch (Exception e) {
-			System.out.println("--->"+e.getMessage());
+			System.out.println("--->" + e.getMessage());
 		}
 
 		try {
 			System.out.println("\napagando evento");
+
+			// Apagando o evento de id 3
 			Fachada.apagarEvento(3);
 
+			// Apagando o participante de cpf "777"
 			System.out.println("\napagando participante");
 			Fachada.apagarParticipante("7777");
 
+			// Apagando ingressos a partir do código dos mesmos
 			System.out.println("\napagando ingresssos");
 			Fachada.apagarIngresso("1-1111");
 			Fachada.apagarIngresso("2-6666");
 
 		} catch (Exception e) {
-			System.out.println("--->"+e.getMessage());
+			System.out.println("--->" + e.getMessage());
 		}
 
 		try {
 			System.out.println("\n---------listagem de participantes - final-----");
-			for(Participante p : Fachada.listarParticipantes()) 
+
+			// Listando os participantes após as inserções/remoções
+			for (Participante p : Fachada.listarParticipantes())
 				System.out.println(p);
 
 			System.out.println("\n---------listagem de eventos - final");
-			for(Evento e : Fachada.listarEventos()) 
+
+			// Listando os eventos após as inserções/remoções
+			for (Evento e : Fachada.listarEventos())
 				System.out.println(e);
 
 			System.out.println("\n---------listagem de ingressos - final");
-			for(Ingresso ing : Fachada.listarIngressos()) 
-				System.out.println(
-						"cod="+ing.getCodigo()+ ", "+ ing.getTelefone() + ", preco=" +	ing.calcularPreco() + 
-						", evento preco=" + ing.getEvento().getPreco() + ", arrecadado=" + ing.getEvento().totalArrecadado() + 
-						", idade=" + ing.getParticipante().calcularIdade());
 
+			// Listando os ingressos após as inserções/remoções
+			for (Ingresso ing : Fachada.listarIngressos())
+				System.out.println(
+						"cod=" + ing.getCodigo() + ", " + ing.getTelefone() + ", preco=" + ing.calcularPreco() +
+								", evento preco=" + ing.getEvento().getPreco() + ", arrecadado="
+								+ ing.getEvento().totalArrecadado() +
+								", idade=" + ing.getParticipante().calcularIdade());
 
 		} catch (Exception e) {
-			System.out.println("--->"+e.getMessage());
-		}	
-
+			System.out.println("--->" + e.getMessage());
+		}
 	}
 
-	public static void main (String[] args) 
-	{
-		new Teste1();
+	public static void main(String[] args) {
+		new Teste1(); // Executando o teste1
 	}
 }
-
-
--- src/appconsole/Teste2.java
+++ src/appconsole/Teste2.java
@@ -1,106 +1,147 @@
 package appconsole;
-/**
- * SI - POO - Prof. Fausto Ayres
- * Teste da Fachada
- *
- */
 
 import regras_negocio.Fachada;
 
 public class Teste2 {
 
-	public static void main (String[] args) {
-		//dados para teste
+	public static void main(String[] args) {
+
+		// Dados para teste
 		String cpf1 = "91";
 		String cpf2 = "92";
 		String cpf3 = "93";
-		int id=1;
+		int id = 1;
+
 		try {
-			
-			Fachada.criarParticipante(cpf1,"01/01/2000");
-			Fachada.criarParticipante(cpf2,"01/01/2000");
-			Fachada.criarConvidado(cpf3,"01/01/2000", "xxxxx");
+			// Criando participates para realização dos testes abaixo
+			Fachada.criarParticipante(cpf1, "01/01/2000");
+			Fachada.criarParticipante(cpf2, "01/01/2000");
+
+			// Criando convidado para realização dos testes abaixo
+			Fachada.criarConvidado(cpf3, "01/01/2000", "xxxxx");
+
+			// Criando evento para realização dos testes abaixo
 			Fachada.criarEvento("01/01/2024", "teste", 2, 2);
-		}catch (Exception e) {System.out.println("dados de teste--->"+e.getMessage());}
+		} catch (Exception e) {
+			System.out.println("dados de teste--->" + e.getMessage());
+		}
 
-		System.out.println("\n-------TESTE DE EXCE��ES LAN�ADAS PELOS METODOS DA FACHADA--------");
+		System.out.println("\n-------TESTE DE EXCEÇÕES LANÇADAS PELOS MÉTODOS DA FACHADA--------");
+
+		// Deve lançar exceção de participante duplicado
 		try {
-			Fachada.criarParticipante(cpf1,"01/01/2000");
-			Fachada.criarParticipante(cpf1,"01/01/2000");
-			System.out.println("*************1--->Nao lan�ou exce��o para: criar participante duplicado "); 
-		}catch (Exception e) {System.out.println("1ok--->"+e.getMessage());}
+			Fachada.criarParticipante(cpf1, "01/01/2000");
+			Fachada.criarParticipante(cpf1, "01/01/2000");
+			System.out.println("*************1--->Não lançou exceção para: criar participante duplicado ");
+		} catch (Exception e) {
+			System.out.println("1ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um participante sem data
 		try {
-			Fachada.criarParticipante("1000","");
-			System.out.println("*************2--->Nao lan�ou exce��o para: criar participante sem data "); 
-		}catch (Exception e) {System.out.println("2ok--->"+e.getMessage());}
+			Fachada.criarParticipante("1000", "");
+			System.out.println("*************2--->Não lançou exceção para: criar participante sem data ");
+		} catch (Exception e) {
+			System.out.println("2ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um convidado sem empresa
 		try {
-			Fachada.criarConvidado("1001","01/01/2000", "");
-			System.out.println("*************3--->Nao lan�ou exce��o para: criar convidado sem empresa"); 
-		}catch (Exception e) {System.out.println("3ok--->"+e.getMessage());}
+			Fachada.criarConvidado("1001", "01/01/2000", "");
+			System.out.println("*************3--->Não lançou exceção para: criar convidado sem empresa");
+		} catch (Exception e) {
+			System.out.println("3ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um evento sem data
 		try {
 			Fachada.criarEvento("", "teste", 10, 10);
-			System.out.println("*************4--->Nao lan�ou exce��o para: criar evento sem data"); 
-		}catch (Exception e) {System.out.println("4ok--->"+e.getMessage());}
+			System.out.println("*************4--->Não lançou exceção para: criar evento sem data");
+		} catch (Exception e) {
+			System.out.println("4ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um evento sem descrição
 		try {
 			Fachada.criarEvento("01/01/2000", "", 10, 10);
-			System.out.println("*************5--->Nao lan�ou exce��o para: criar evento sem descricao"); 
-		}catch (Exception e) {System.out.println("5ok--->"+e.getMessage());}
+			System.out.println("*************5--->Não lançou exceção para: criar evento sem descricao");
+		} catch (Exception e) {
+			System.out.println("5ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um evento com capacidade mínima menor do que 2
 		try {
 			Fachada.criarEvento("01/01/2000", "teste", 1, 10);
-			System.out.println("*************6--->Nao lan�ou exce��o para: criar evento sem capacidade minima"); 
-		}catch (Exception e) {System.out.println("6ok--->"+e.getMessage());}
+			System.out.println("*************6--->Não lançou exceção para: criar evento sem capacidade mínima");
+		} catch (Exception e) {
+			System.out.println("6ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um evento com preço negativo
 		try {
 			Fachada.criarEvento("01/01/2000", "teste", 2, -1);
-			System.out.println("*************7--->Nao lan�ou exce��o para: criar evento com preco negativo"); 
-		}catch (Exception e) {System.out.println("7ok--->"+e.getMessage());}
+			System.out.println("*************7--->Não lançou exceção para: criar evento com preco negativo");
+		} catch (Exception e) {
+			System.out.println("7ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um ingresso cujo id do evento não existe
 		try {
-			Fachada.criarIngresso(99,cpf1, "999999999");
-			System.out.println("*************8--->Nao lan�ou exce��o para: criar ingresso com id inexistente"); 
-		}catch (Exception e) {System.out.println("8ok--->"+e.getMessage());}
+			Fachada.criarIngresso(99, cpf1, "999999999");
+			System.out.println("*************8--->Não lançou exceção para: criar ingresso com id inexistente");
+		} catch (Exception e) {
+			System.out.println("8ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar ingresso com cpf inexistente
 		try {
-			Fachada.criarIngresso(id,"9999000", "999999999");
-			System.out.println("*************9--->Nao lan�ou exce��o para: criar ingresso com cpf inexistente"); 
-		}catch (Exception e) {System.out.println("9ok--->"+e.getMessage());}
+			Fachada.criarIngresso(id, "9999000", "999999999");
+			System.out.println("*************9--->Não lançou exceção para: criar ingresso com cpf inexistente");
+		} catch (Exception e) {
+			System.out.println("9ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar ingresso sem número de telefone
 		try {
-			Fachada.criarIngresso(id,cpf1, "");
-			System.out.println("*************10--->Nao lan�ou exce��o para: criar ingresso com telefone inexistente"); 
-		}catch (Exception e) {System.out.println("10ok--->"+e.getMessage());}
+			Fachada.criarIngresso(id, cpf1, "");
+			System.out.println("*************10--->Não lançou exceção para: criar ingresso com telefone inexistente");
+		} catch (Exception e) {
+			System.out.println("10ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao criar um ingresso duplicado
 		try {
-			Fachada.criarIngresso(id,cpf1, "999999999");
-			Fachada.criarIngresso(id,cpf1, "999999999");
-			System.out.println("*************11--->Nao lan�ou exce��o para: criar ingresso duplicado"); 
-		}catch (Exception e) {System.out.println("11ok--->"+e.getMessage());}
+			Fachada.criarIngresso(id, cpf1, "999999999");
+			Fachada.criarIngresso(id, cpf1, "999999999");
+			System.out.println("*************11--->Não lançou exceção para: criar ingresso duplicado");
+		} catch (Exception e) {
+			System.out.println("11ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao tenter criar um ingresso num evento que já alcançou a
+		// capacidade máxima
 		try {
-			Fachada.criarIngresso(id,cpf2, "999999999");
-			Fachada.criarIngresso(id,cpf3, "999999999");
-			System.out.println("*************12--->Nao lan�ou exce��o para: criar ingresso alem da capacidade"); 
-		}catch (Exception e) {System.out.println("12ok--->"+e.getMessage());}
+			Fachada.criarIngresso(id, cpf2, "999999999");
+			Fachada.criarIngresso(id, cpf3, "999999999");
+			System.out.println("*************12--->Não lançou exceção para: criar ingresso alem da capacidade");
+		} catch (Exception e) {
+			System.out.println("12ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao tentar apagar um evento que ainda possui ingressos
 		try {
 			Fachada.apagarEvento(id);
-			System.out.println("*************13--->Nao lan�ou exce��o para: apagar evento ainda com ingresso"); 
-		}catch (Exception e) {System.out.println("13ok--->"+e.getMessage());}
+			System.out.println("*************13--->Nao lançou exceção para: apagar evento ainda com ingresso");
+		} catch (Exception e) {
+			System.out.println("13ok--->" + e.getMessage());
+		}
 
+		// Deve lançar exceção ao tentar apagar um participante com ingresso em uso
 		try {
 			Fachada.apagarParticipante(cpf1);
-			System.out.println("*************14--->Nao lan�ou exce��o para: apagar participante com ingresso em uso"); 
-		}catch (Exception e) {System.out.println("14ok--->"+e.getMessage());}
-
-
-
-
+			System.out.println("*************14--->Não lançou exceção para: apagar participante com ingresso em uso");
+		} catch (Exception e) {
+			System.out.println("14ok--->" + e.getMessage());
+		}
 	}
 }
-
-
